c267c353193491d44efd64767d75ae3860c2e3a8
sebastianpergo <sebastianperdomopg@gmail.com>
2023-06-22 22:52:36 -0500

    feat(FirstComponent): Added an array and shown the array

--- demo-app/src/components/FirstComponent.js
+++ demo-app/src/components/FirstComponent.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 export const FirstComponent = () => {
-  return (
-    <div>FirstComponent
+    // Create a variable called name and assign it the value of your name
+    let name = "Sebastian"
+    let lastName = "Perdomo"
+    let age = 40
+
+    let cursos = ["React", "Angular", "Vue"]
+  
+    return (
+    <div>About me
+        <h1>My Data</h1>
+        <p>My name is: {name}</p>
+        <p>My last name is: {lastName}</p>
+        <p>My age is: {age}</p>
+        <h2>My courses</h2>
         <ul>
-            <li>First</li>
-            <li>Second</li>
-            <li>Third</li>
+            {cursos.map((curso, index) => {
+                return <li key={index}>{curso}</li>
+            })}
         </ul>
+
+
     </div>
   )
 }
