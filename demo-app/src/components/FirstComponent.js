6fd6446e952cc475e876ba1f3b2a6dac6e5c7bcc
sebastianpergo <sebastianperdomopg@gmail.com>
2023-06-22 22:42:16 -0500

    feat(FirstComponent and SecondComponent): Added both componentes to the project

---
+++ demo-app/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const FirstComponent = () => {
+  return (
+    <div>FirstComponent
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+        </ul>
+    </div>
+  )
+}
+
---
+++ demo-app/src/components/SecondComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SecondComponent = () => {
+  return (
+    <div>SecondComponent</div>
+  )
+}
